7609c33240b8b77c5f46c617b3eb0d53c4eceeae
yaroslavpankovych <flower.moor@gmail.com>
2020-04-17 18:26:40 +0300

    added external_call

--- src/rest_client/utils/__init__.py
+++ src/rest_client/utils/__init__.py
@@ -1,3 +1,4 @@
+import contextlib
 import datetime
 import functools
 
@@ -18,3 +19,31 @@ def with_rest_client(rest_client, wrap_response=True):
         return inner
 
     return wrapper
+
+
+class WrappedRestClient:
+
+    def __init__(self, rest_client):
+        self.data = None
+        self.status = None
+        self.rest_client = rest_client
+
+    def __getattr__(self, item):
+        self._method = item
+        return self.patched
+
+    async def patched(self, *args, **kwargs):
+        method = getattr(self.rest_client, self._method)
+        response = await method(*args, **kwargs)
+        self.data, self.status = response
+        return self.data, self.status
+
+
+@contextlib.contextmanager
+def external_call(rest_client, allowed_statuses=(200,), error_message="Service unavailable."):
+    wrapped_client = WrappedRestClient(rest_client)
+    try:
+        yield wrapped_client
+    finally:
+        if wrapped_client.status not in allowed_statuses:
+            raise Exception(error_message)
