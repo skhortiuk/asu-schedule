eff95dff95ca6d7f3bf0f91d724a58a31e0edcd2
Serhii Khortiuk <khortiukserhii@ukr.net>
2020-04-17 19:00:50 +0300

    Added groups and teachers controllers

--- src/controllers/group_controller.py
+++ src/controllers/group_controller.py
@@ -6,14 +6,13 @@ from src.rest_client.utils import with_rest_client, external_call
 
 @with_rest_client(GroupRestClient)
 async def get_all_groups(client: GroupRestClient, query: Optional[str] = None, faculty: Optional[int] = None, **kwargs):
-    with external_call(client) as client:
-        data, status = await client.all_groups(query=query, faculty=faculty)
-    return data
+    with external_call(client) as rest_client:
+        data, status = await rest_client.all_groups(query=query, faculty=faculty)
+    return data, status
 
 
 @with_rest_client(GroupRestClient)
 async def is_group_exists(client: GroupRestClient, query: str, faculty: Optional[int] = None, **kwargs):
     with external_call(client) as rest_client:
         data, status = await rest_client.exists(group_code=query, faculty=faculty)
-        print(data, status)
-    return data
+    return data, status
