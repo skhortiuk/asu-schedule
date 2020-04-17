663970b324be2987d629d2e9d6c89a3ca0b569ba
yaroslavpankovych <flower.moor@gmail.com>
2020-04-17 20:28:57 +0300

    customize swagger

--- src/app.py
+++ src/app.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI
+from fastapi.openapi.utils import get_openapi
 
 from src.routesrs import groups, teachers, schedule, faculties
 
 app = FastAPI()
 
+
+def custom_open_api():
+    if app.openapi_schema:
+        return app.openapi_schema
+    open_api_schema = get_openapi(
+        title="Ultimate Schedule API",
+        version="1.0.0",
+        description="Provides you possibility to interact with different university's schedules.",
+        routes=app.routes,
+    )
+    app.openapi_schema = open_api_schema
+    return app.openapi_schema
+
+
+app.openapi = custom_open_api
 app.include_router(groups.router, prefix="/groups")
 app.include_router(teachers.router, prefix="/teachers")
 app.include_router(schedule.router, prefix="/schedule")
