da68288325fdbdb00c1ab4386f8d907f0a9b2590
yaroslavpankovych <flower.moor@gmail.com>
2020-04-17 20:16:26 +0300

    added schedule for teachers

--- src/controllers/schedule_controller.py
+++ src/controllers/schedule_controller.py
@@ -1,5 +1,6 @@
 from src.common.parse import parse
 from src.rest_client.groups_schedule import GroupScheduleRestClient
+from src.rest_client.teachers_schedule import TeacherScheduleRestClient
 from src.rest_client.utils import with_rest_client, external_call
 
 
@@ -7,4 +8,11 @@ from src.rest_client.utils import with_rest_client, external_call
 async def get_group_schedule(client: GroupScheduleRestClient, from_date: str, to_date: str, group: str, **kwargs):
     with external_call(client) as rest_client:
         data, status = await rest_client.schedule(from_date=from_date, to_date=to_date, value=group)
+    return parse(data), status
+
+
+@with_rest_client(TeacherScheduleRestClient)
+async def get_teacher_schedule(client: GroupScheduleRestClient, from_date: str, to_date: str, teacher: str, **kwargs):
+    with external_call(client) as rest_client:
+        data, status = await rest_client.schedule(from_date=from_date, to_date=to_date, value=teacher)
     return parse(data), status
--- src/routesrs/schedule.py
+++ src/routesrs/schedule.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from src.controllers.schedule_controller import get_group_schedule
+from src.controllers.schedule_controller import get_group_schedule, get_teacher_schedule
 from src.routesrs.schema import x_schedule_header
 
 router = APIRouter()
@@ -13,4 +13,14 @@ async def groups_schedule(
 ):
     return await get_group_schedule(
         from_date=date_from, to_date=date_to, group=value, schedule_url=schedule_url
+    )
+
+
+@router.get("/teachers")
+async def teachers_schedule(
+        *, value: str, date_from: str, date_to: str,
+        schedule_url: str = x_schedule_header
+):
+    return await get_teacher_schedule(
+        from_date=date_from, to_date=date_to, teacher=value, schedule_url=schedule_url
     )
