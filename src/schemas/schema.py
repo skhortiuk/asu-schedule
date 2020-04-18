41e6482822c5bcf4f0d6ab80aad21a9e97f9226e
Serhii Khortiuk <khortiukserhii@ukr.net>
2020-04-18 19:06:23 +0300

    Fixed typo

--- src/schemas/schema.py
+++ src/schemas/schema.py
@@ -1,6 +1,6 @@
 from fastapi import Header
 from validators.url import regex
 
-from src.routesrs.config import SCHEDULE_HEADER_NAME
+from src.routers.config import SCHEDULE_HEADER_NAME
 
 x_schedule_header = Header(..., alias=SCHEDULE_HEADER_NAME, regex=regex.pattern)
