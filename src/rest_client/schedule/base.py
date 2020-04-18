7ab3db63c22c8ae11646129ffc7408bbaadc774b
yaroslavpankovych <flower.moor@gmail.com>
2020-04-18 18:17:56 +0300

    black

--- src/rest_client/schedule/base.py
+++ src/rest_client/schedule/base.py
@@ -8,11 +8,13 @@ class ScheduleBaseRestClient(BaseRestClient):
     ENTITY: NotImplemented
 
     async def schedule(
-            self, from_date: str, to_date: str, value: str, headers: Dict = None
+        self, from_date: str, to_date: str, value: str, headers: Dict = None
     ) -> api_response:
         payload = {
             "sdate": from_date.encode(DEFAULT_ENCODING) if from_date else "",
             "edate": to_date.encode(DEFAULT_ENCODING) if to_date else "",
             self.ENTITY: value.encode(DEFAULT_ENCODING),
         }
-        return await self.post(headers=headers, data=payload, params={"n": SCHEDULE_FLAG})
+        return await self.post(
+            headers=headers, data=payload, params={"n": SCHEDULE_FLAG}
+        )
