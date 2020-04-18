71c70b2d75e1dd29c55e94406e60a248e59bad2a
yaroslavpankovych <flower.moor@gmail.com>
2020-04-18 18:17:17 +0300

    added schedule flag

--- src/rest_client/schedule/base.py
+++ src/rest_client/schedule/base.py
@@ -1,18 +1,18 @@
 from typing import Dict
 
 from src.rest_client.base import BaseRestClient, api_response
-from src.rest_client.config import DEFAULT_ENCODING
+from src.rest_client.config import DEFAULT_ENCODING, SCHEDULE_FLAG
 
 
 class ScheduleBaseRestClient(BaseRestClient):
     ENTITY: NotImplemented
 
     async def schedule(
-        self, from_date: str, to_date: str, value: str, headers: Dict = None
+            self, from_date: str, to_date: str, value: str, headers: Dict = None
     ) -> api_response:
         payload = {
             "sdate": from_date.encode(DEFAULT_ENCODING) if from_date else "",
             "edate": to_date.encode(DEFAULT_ENCODING) if to_date else "",
             self.ENTITY: value.encode(DEFAULT_ENCODING),
         }
-        return await self.post(headers=headers, data=payload)
+        return await self.post(headers=headers, data=payload, params={"n": SCHEDULE_FLAG})
